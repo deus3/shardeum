8a0f09e493a1ed05dcff145f704552717c7a69cb
deus3 <133024101+deus3@users.noreply.github.com>
2024-05-17 16:51:04 +0200

    Update client.sh

--- client.sh
+++ client.sh
@@ -4,4 +4,4 @@ echo "Shell commands:"
 echo "Start node:        operator-cli gui start; operator-cli start; exit"
 echo "Status:            operator-cli gui status; operator-cli status;"
 cd ~/.shardeum
-./shell.sh 
+./shell.sh  
